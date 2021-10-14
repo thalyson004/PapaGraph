a359f2ab7392e63fb38179d5331c6b5c2faa60dd
Thalyson <thalyson.uece@gmail.com>
2021-10-14 14:01:24 -0300

    finish random_walk function

--- PapaGraph/src/PapaGraph/walk/random_walk.py
+++ PapaGraph/src/PapaGraph/walk/random_walk.py
@@ -1,37 +1,54 @@
 from PapaGraph.PapaGraph.PapaGraph import PapaGraph
+from PapaGraph.algorithm.shortest_path import steps_from
+from PapaGraph.sample.handmande import handmade_sample
+import random
 
-def random_walk(graph:PapaGraph, start:int, lenght:int, p:float=0.5, q:float=None):
+# TODO: If i'm on node with no neighbors, actual algorithm not work properly
+def random_walk(graph:PapaGraph, start:int, lenght:int, p:float=1.0, q:float=1.0):
     '''Generate a random_walk starting.
     `start`: Start node
     `lenght`: Lenght of the walk
     `p`: Return paramenter
-    `q`: Walk away paramenter
+    `q`: In-Out paramenter
     '''
     assert start>=0 and start<graph.nodes_number
-    assert p!=None and p>=0.0 and p<=1.0
-
-    if q==None:
-        q = 1.0-p
-    else:
-        assert p+q==1.0
+    assert isinstance(p, float)
+    assert isinstance(q, float)
     
     walk:list = [start]
+    prev:int = None
+    actual:int = start
+
+    steps_distance:list = steps_from(graph=graph, start=start) 
+
+    for step in range(lenght):
+        neighbors:list[tuple] = graph.neighbors(actual)
+        probability_sum:float = 0.0
+        probabilities:list[float] = []
+
+        for neighbor_index, edge_index in neighbors:
+            if neighbor_index == prev:
+                probabilities.append(1.0/p)
+            elif steps_distance[neighbor_index] > steps_distance[actual]:
+                probabilities.append(1.0/q)
+            else: 
+                probabilities.append(1.0)
+
+        probability_sum:float = sum(probabilities)
+
+        probability_remain:float = random.uniform(0,probability_sum)
+        prev = actual
+        for i, (neighbor_index, edge_index) in enumerate(neighbors):
+            if probability_remain <= probabilities[i]:
+                actual = neighbor_index
+                break
+            else:
+                probability_remain = probability_remain - probabilities[i]
+        
+        walk.append(actual)
 
     return walk    
 
 if __name__ == '__main__':
-    graph = PapaGraph()
-    graph.add_nodes(quantity=8)
-    graph.add_edge(0,1, bidirectional=True)
-    graph.add_edge(0,2, bidirectional=True)
-    graph.add_edge(0,3, bidirectional=True)
-    graph.add_edge(1,2, bidirectional=True)
-    graph.add_edge(1,7, bidirectional=True)
-    graph.add_edge(2,3, bidirectional=True)
-    graph.add_edge(3,4, bidirectional=True)
-    graph.add_edge(4,7, bidirectional=True)
-    graph.add_edge(4,5, bidirectional=True)
-    graph.add_edge(4,6, bidirectional=True)
-    graph.add_edge(5,6, bidirectional=True)
-    
-    print(random_walk(graph, 0, 3))
+    graph = handmade_sample()
+    print(random_walk(graph, 0, 5))
