d714b4bf71735b921bedd83c58d83b8250e5ba0b
Thalyson <thalyson.uece@gmail.com>
2021-10-26 12:54:49 -0300

    Add assert messages

--- PapaGraph/src/PapaGraph/walk/random_walk.py
+++ PapaGraph/src/PapaGraph/walk/random_walk.py
@@ -18,9 +18,9 @@ def random_walk(graph:PapaGraph, start:int, lenght:int, p:float=1.0, q:float=1.0
     ----
     `walk`: A list of nodes representing a random walk
     '''
-    assert start>=0 and start<graph.nodes_number
-    assert isinstance(p, float)
-    assert isinstance(q, float)
+    assert start>=0 and start<graph.nodes_number, "node is not in graph"
+    assert isinstance(p, float), "p parameter must by float32"
+    assert isinstance(q, float), "q parameter must by float32"
     
     walk:list = [start]
     prev:int = None
@@ -72,13 +72,16 @@ def random_walk_r(graph:PapaGraph, start:int, r:int=3, lenght:int=10, p:float=1.
     ----
     `walks`: A list of lists of nodes representing random walks
     '''
+    assert start>=0 and start<graph.nodes_number, "node is not in graph"
+    assert isinstance(p, float), "p parameter must by float32"
+    assert isinstance(q, float), "q parameter must by float32"
 
     walks = []
     for i in range(r):
         walks.append(random_walk(graph, start, lenght, p, q))
     return walks
 
-def random_walk_r_all(graph:PapaGraph, r:int=3, lenght:int=10, p:float=1.0, q:float=1.0): 
+def random_walk_r_all(graph:PapaGraph, lenght:int=10, r:int=3, p:float=1.0, q:float=1.0): 
     '''Generate ``r`` random walks per node.
     
     Input
@@ -93,6 +96,8 @@ def random_walk_r_all(graph:PapaGraph, r:int=3, lenght:int=10, p:float=1.0, q:fl
     ----
     `walks`: A list of lists of nodes representing random walks
     '''
+    assert isinstance(p, float), "p parameter must by float32"
+    assert isinstance(q, float), "q parameter must by float32"
 
     walks = []
     for node in range(graph.nodes_number):
